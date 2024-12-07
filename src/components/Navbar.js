9156ebab86e53d6015e98517d2bf82e260f8e2ef
Siam <ahmedsiam037@gmail.com>
2024-12-07 15:04:28 +1100

    fixed hololive label

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <div className="header">
       <img src="Hololive-logo.png" alt="Hololive logo" className="logo" />
-      <h1>HoloLive Talent Selector</h1>
+      <h1>Hololive Talent Selector</h1>
     </div>
   );
 }
